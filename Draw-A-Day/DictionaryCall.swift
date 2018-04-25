417f4c1ffef232287985eb46048ae513ccabf27d
Andrew Lau <andrewlau@Andrews-MacBook-Pro-3.local>
2018-04-25 15:20:25 -0700

    double api call and random word functioning

--- Draw-A-Day/DictionaryCall.swift
+++ Draw-A-Day/DictionaryCall.swift
@@ -9,40 +9,88 @@
 import Foundation
 
 struct Root : Decodable {
+    let metadata : MetaData
     let results : [Results]
 }
 
-    
+struct MetaData: Decodable{
+    let total: Int
+}
+
+
 struct Results: Decodable{
-        let id: String
-        let word: String
+    let id: String
+    let word: String
+}
+
+class API {
     
+    var count: Int = 0
     
-    static func dictCall(completion: @escaping (String) -> ()){
-        // TODO: replace with your own app id and app key
+    func randomOffset(count: Int) -> Int {
+        print("random count is " + "\(count)")
+        let rand = Int(arc4random_uniform(UInt32(count)));
+        print("the random num is " + "\(rand)")
+        return rand
+        
+    }
+    
+    init(){
         let appId = "111aab88"
         let appKey = "78ff37165f7543bcb86ee546b7e8e8c3"
         let language = "en"
         let filters = "lexicalCategory=noun;domains=Clothing"
-        let endpoint = "lexicalCategory"
-        let limit = "?limit=1"
-        let offset = "&offset=359"
-        let jsonUrlString = "https://od-api.oxforddictionaries.com:443/api/v1/wordlist/\(language)/\(filters)\(limit)\(offset)"
-        //let url = URL(string: "https://od-api.oxforddictionaries.com:443/api/v1/domains /\(language)")!
-        
-        
-        guard let url = URL(string: jsonUrlString) else {return}
+        let limit = "?limit=2"
+        let jsonUrlString = "https://od-api.oxforddictionaries.com:443/api/v1/wordlist/\(language)/\(filters)\(limit)"
+        var url : URL!
+        var count:Int = 0
+        url = URL(string: jsonUrlString)
         var request = URLRequest(url: url)
         request.addValue("application/json", forHTTPHeaderField: "Accept")
         request.addValue(appId, forHTTPHeaderField: "app_id")
         request.addValue(appKey, forHTTPHeaderField: "app_key")
         URLSession.shared.dataTask(with: request, completionHandler: { (data, response, err) in
             
-            var noun:String = ""
+            guard let data = data else {return}
+            
+            do {
+                let root = try JSONDecoder().decode(Root.self, from : data)
+                if let lr = try? root.metadata.total{ self.count = lr}
+            } catch let jsonErr {
+                print("Error serializaing json:", jsonErr)
+            }
+            print("count is " + "\(self.count)")
+        }).resume()
+        
+    }
+    
+    func dictCall(completion: @escaping (String) -> ()){
+        var noun: String = ""
+        
+        let newOffset = randomOffset(count: self.count)
+        print("offset is " + "\(newOffset)")
+        let appId = "111aab88"
+        let appKey = "78ff37165f7543bcb86ee546b7e8e8c3"
+        let language = "en"
+        let filters = "lexicalCategory=noun;domains=Clothing"
+        let limit = "?limit=1"
+        let offset2 = "&offset=\(newOffset)"
+        let jsonUrlString = "https://od-api.oxforddictionaries.com:443/api/v1/wordlist/\(language)/\(filters)\(limit)\(offset2)"
+        var url : URL!
+        
+        url = URL(string: jsonUrlString)
+        var request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue(appId, forHTTPHeaderField: "app_id")
+        request.addValue(appKey, forHTTPHeaderField: "app_key")
+        
+        URLSession.shared.dataTask(with: request, completionHandler: { (data, response, err) in
+            
             
             guard let data = data else {return}
             
             do {
+                print(data.count)
                 let root = try JSONDecoder().decode(Root.self, from : data)
                 
                 if let wordObject = try? root.results[0].word {
@@ -61,4 +109,5 @@ struct Results: Decodable{
         }).resume()
         return
     }
+
 }
--- Draw-A-Day/GetWord.swift
+++ Draw-A-Day/GetWord.swift
@@ -4,12 +4,14 @@ import UIKit
 class getWord: UIViewController {
     
     var txt = "wgaaa"
+    var api = API()
     
     @IBOutlet weak var Noun: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         Noun.text = txt
+        
         
 
         
@@ -18,7 +20,7 @@ class getWord: UIViewController {
 
     
     @IBAction func getWordTap(_ sender: Any) {
-        Results.dictCall() { (results:String) in
+        api.dictCall() { (results:String) in
             print(results);
             DispatchQueue.main.async {
                 //Update your UI
@@ -26,6 +28,31 @@ class getWord: UIViewController {
             }
             
         }
+ 
+        /*
+        let appId = "111aab88"
+        let appKey = "78ff37165f7543bcb86ee546b7e8e8c3"
+        let language = "en"
+        let filters = "domains=Art"
+        let url = URL(string: "https://od-api.oxforddictionaries.com:443/api/v1/wordlist/en/lexicalCategory%3DNoun%3Bdomains%3Dsport?word_length=%3E5%2C%3C10&exact=false&limit=1")!
+        var request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue(appId, forHTTPHeaderField: "app_id")
+        request.addValue(appKey, forHTTPHeaderField: "app_key")
+        
+        let session = URLSession.shared
+        _ = session.dataTask(with: request, completionHandler: { data, response, error in
+            if let response = response,
+                let data = data,
+                let jsonData = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) {
+                
+                print(jsonData)
+            } else {
+                print(error)
+                print(NSString.init(data: data!, encoding: String.Encoding.utf8.rawValue))
+            }
+        }).resume()
+        */
     }
     
     
